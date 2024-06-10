d10029c3159d9e3d35e746933b78242f87bd416d
Amru Abid Zakly <amruabid8212@gmail.com>
2024-06-10 17:05:23 +0700

    merge with main

--- app/src/main/java/org/d3ifcool/virtualab/ui/screen/guru/latihan/GuruLatihanViewModel.kt
+++ app/src/main/java/org/d3ifcool/virtualab/ui/screen/guru/latihan/GuruLatihanViewModel.kt
@@ -7,7 +7,7 @@ class GuruLatihanViewModel: ViewModel() {
     val data = getDataDummy()
     private fun getDataDummy(): List<Materi> {
         val data = mutableListOf<Materi>()
-//        for (i in 29 downTo 20) {
+        for (i in 29 downTo 20) {
 //            data.add(
 //                Materi(
 //                    i.toLong(),
@@ -17,7 +17,7 @@ class GuruLatihanViewModel: ViewModel() {
 //                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ultricies lectus nec ultricies accumsan. Praesent consectetur rhoncus metus, in vestibulum nulla. Quisque vehicula lectus auctor, cursus nunc mollis, aliquet mi. In sollicitudin, augue quis posuere venenatis, ex nulla scelerisque massa, non semper augue velit a odio. "
 //                )
 //            )
-//        }
+        }
         return data
     }
 }
